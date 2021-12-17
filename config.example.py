5a27024cbed92bd6a1cc10fb9bb2eed45f664b46
Timm Simpkins <timm@talova.com>
2021-12-17 18:34:31 -0500

    Edited config.py

--- config.example.py
+++ config.example.py
@@ -1,7 +1,5 @@
 import digitalocean
-from datetime import datetime
 
-now = datetime.now()
 # Add your digital ocean API token
 manager = digitalocean.Manager(token="DO_TOKEN")
 
@@ -10,6 +8,7 @@ domain_port_droplet = [
     # ["DOMAIN", PORT#, DROPLET_ID],
 ]
 
+# Do not edit below this line
 for i in range(len(domain_port_droplet)):
     domain, port, droplet = domain_port_droplet[i]
     domain_port_droplet[i] = [domain, port, manager.get_droplet(droplet_id=str(droplet)), None]
