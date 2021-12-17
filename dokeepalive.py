02b7b8a64d4cbd4b59fdc8ba20d2ed741e17d06a
Timm Simpkins <timm@talova.com>
2021-12-17 17:59:50 -0500

    Added comments

--- dokeepalive.py
+++ dokeepalive.py
@@ -12,12 +12,17 @@ def port_is_open(domain, port):
 
 def main():
     while True:
+        # Iterate over domains
         for i in range(len(domain_port_droplet)):
             (domain, port, droplet, last_checked) = domain_port_droplet[i]
+            # Check if domain has been tested already, set time to 5 minutes ago so it will start immediately.
             if last_checked is None:
                 last_checked = domain_port_droplet[i][3] = datetime.now() - timedelta(minutes=5)
+            # If the last time domain was checked was over 5 minutes ago, check again.
             if datetime.now() - last_checked > timedelta(minutes=5):
+                # Reset the last checked time to now.
                 domain_port_droplet[i][3] = datetime.now()
+                # If the port is not open, server is down, so restart.
                 if not port_is_open(domain, port):
                     now = datetime.now()
                     droplet.reboot()
