ad87d87c24b8d124cdf81f5c82913f5693c32574
Timm Simpkins <timm@talova.com>
2021-12-17 20:19:30 -0500

    Added frequency to config setup script.

--- create_config.py
+++ create_config.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 from string import Template
 
+frequency = input("Enter interval in minutes to poll server status: ")
 DO_Token = input("Enter your Digital Ocean API token: ")
 domain_info = []
 while True:
-    print("For each domain, enter the domain, the port to check, and the droplet ID.  When finished, enter a blank line on the domain line to stop.")
+    print("For each domain, enter the domain, the port to check, and the droplet ID."
+          "  When finished, enter a blank line on the domain line to stop.")
     domain = input("Domain (blank to stop input): ")
     if not domain:
         break
@@ -19,8 +21,9 @@ if domain_info:
         domain_list += '    [' + ', '.join(domain) + '],\n'
     domain_list += ']'
     sub = {
+        'FREQUENCY': frequency,
         'DO_TOKEN': DO_Token,
-        'list': domain_list
+        'LIST': domain_list
     }
     with open('config.template', 'r') as f:
         src = Template(f.read())
