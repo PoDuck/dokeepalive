8f3fd9a8bb5be2e8346ab6ddee369dfaeb466d04
Timm Simpkins <timm@talova.com>
2021-12-21 11:22:44 -0500

    Changed to allow for unlimited tokens and sites.  Converted to class structure for easier management of sites.

--- dokeepalive.py
+++ dokeepalive.py
@@ -1,6 +1,26 @@
-from config import frequency, domain_port_droplet
-from datetime import datetime, timedelta
+import logging
+from datetime import timedelta
 from scapy.all import *
+import digitalocean
+import json
+import re
+
+
+# logging.basicConfig(filename="dokeepalive.log", filemode="a", format='%(asctime)s - %(message)s', level=logging.INFO)
+formatter = logging.Formatter('%asctime)s %(levelname)s %(message)s')
+
+
+def logger_setup(name, log_file, level=logging.INFO):
+    handler = logging.FileHandler(log_file)
+    handler.setFormatter(formatter)
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+    return logger
+
+
+logger = logger_setup('info_logger', 'access.log')
+error_logger = logger_setup('error_logger', 'error.log', logging.ERROR)
 
 
 def port_is_open(domain, port):
@@ -14,24 +34,70 @@ def port_is_open(domain, port):
         return False
 
 
+class Site(object):
+    def __init__(self, site_data, manager):
+        self.site_data = site_data
+        self.manager = manager
+        self.droplet_id = str(site_data['droplet_id'])
+        self.droplet = self.manager.get_droplet(droplet_id=self.droplet_id)
+        self.host = site_data['host']
+        self.port = int(site_data['port'])
+        temp = re.compile("([0-9]+)([a-zA-Z]+)")
+        res = temp.match(site_data['interval']).groups()
+        if res[1] == "s":
+            self.interval = timedelta(seconds=int(res[0]))
+        elif res[1] == "m":
+            self.interval = timedelta(minutes=int(res[0]))
+        elif res[1] == "h":
+            self.interval = timedelta(hours=int(res[0]))
+        elif res[1] == "d":
+            self.interval = timedelta(hours=int(res[0]))
+        self.last_checked = datetime.now() - self.interval
+
+    def reboot(self):
+        self.droplet.reboot()
+
+    def update(self):
+        now = datetime.now()
+        if self.last_checked <= now - self.interval:
+            if not port_is_open(self.host, self.port):
+                error_logger.error("Site: " + self.host + " was reported not responsive")
+                self.reboot()
+            else:
+                logger.info("Site: " + self.host + " was reported open")
+            self.last_checked = now
+
+
+class User(object):
+    def __init__(self, user_data):
+        self.sites = []
+        self.token = user_data['token']
+        self.manager = digitalocean.Manager(token=self.token)
+        for site in user_data['sites']:
+            self.sites.append(Site(site, self.manager))
+
+    def update(self):
+        for site in self.sites:
+            site.update()
+
+
+class UsersAndSites(object):
+    def __init__(self):
+        with open("dokeepalive.conf") as config_data:
+            self.apis = json.load(config_data)
+        self.users = []
+        for user in self.apis:
+            self.users.append(User(user))
+
+    def update(self):
+        for user in self.users:
+            user.update()
+
+
 def main():
+    dka = UsersAndSites()
     while True:
-        # Iterate over domains
-        for i in range(len(domain_port_droplet)):
-            (domain, port, droplet, last_checked) = domain_port_droplet[i]
-            # Check if domain has been tested already, set time to 5 minutes ago so it will start immediately.
-            if last_checked is None:
-                last_checked = domain_port_droplet[i][3] = datetime.now() - timedelta(minutes=frequency)
-            # If the last time domain was checked was over 5 minutes ago, check again.
-            if datetime.now() - last_checked > timedelta(minutes=frequency):
-                # Reset the last checked time to now.
-                domain_port_droplet[i][3] = datetime.now()
-                # If the port is not open, server is down, so restart.
-                if not port_is_open(domain, port):
-                    now = datetime.now()
-                    droplet.reboot()
-                    with open("dokeepalive.log", "a") as logfile:
-                        logfile.write("Site " + domain + " down - " + str(now) + "\n")
+        dka.update()
 
 
 if __name__ == '__main__':
